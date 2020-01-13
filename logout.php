83e3eab37e89818d478ed1d5183c0017c36d820b
weeroshara <weerosharajayasekara@gmail.com>
2020-01-13 10:33:06 +0530

    create session cooki

--- logout.php
+++ logout.php
@@ -3,9 +3,14 @@
 
     $_SESSION=array();
 
-
+    if(isset($_COOKIE[session_name()])){
+        setcookie(session_name(),'',time()-8500,'/');
+    }
+    
     session_destroy();
 
     header('Location: index.php');
+
+
 
 ?>
